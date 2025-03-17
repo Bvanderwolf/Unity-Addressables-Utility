7e15e164604d8aed9c9a0be89c4a0c26022afdd6
Benjamin van der Wolf <34794758+Bvanderwolf@users.noreply.github.com>
2025-03-17 12:15:01 +0100

    Create RequestResult.cs

---
+++ RequestResult.cs
@@ -0,0 +1,50 @@
+public struct DataRequestResult<T>
+{
+    public bool success;
+    public string error;
+    public T data;
+        
+    public static DataRequestResult<T> CreateSuccess(T result)
+    {
+        return new DataRequestResult<T>
+        {
+            success = true,
+            error = string.Empty,
+            data = result
+        };
+    }
+        
+    public static DataRequestResult<T> CreateError(string error)
+    {
+        return new DataRequestResult<T>
+        {
+            success = false,
+            error = error,
+            data = default
+        };
+    }
+}
+
+public struct RequestResult
+{
+    public bool success;
+    public string error;
+        
+    public static RequestResult CreateSuccess()
+    {
+        return new RequestResult
+        {
+            success = true,
+            error = string.Empty
+        };
+    }
+        
+    public static RequestResult CreateError(string error)
+    {
+        return new RequestResult
+        {
+            success = false,
+            error = error
+        };
+    }
+}
